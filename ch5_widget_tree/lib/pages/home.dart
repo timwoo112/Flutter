65533717ed8da0fbeeea93b54bb83418f3b6fc91
timwoo112 <timwoo112@gmail.com>
2020-03-03 13:43:54 -0500

    Added widget tree example

--- ch5_widget_tree/lib/pages/home.dart
+++ ch5_widget_tree/lib/pages/home.dart
@@ -18,49 +18,113 @@ class _HomeState extends State<Home> {
             padding: EdgeInsets.all(16.0),
             child: Column(
               children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Container(
-                      color: Colors.yellow,
-                      height: 40.0,
-                      width: 40.0,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16.0),
-                    ),
-                    Expanded(
-                      child: Container(
-                        color: Colors.amber,
-                        height: 40.0,
-                        width: 40.0,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(16.0),
-                    ),
-                    Container(
-                      color: Colors.brown,
-                      height: 40.0,
-                      width: 40.0,
-                    ),
-                  ],
-                ),
+                _buildHorizontalRow(),
                 Padding(
                   padding: EdgeInsets.all(16.0),
                 ),
-                Row(
-                  children: <Widget>[
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                    )
-                  ],
-                ),
+                _buildRowAndColumn(),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Row _buildRowAndColumn() {
+    return Row(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Container(
+                        color: Colors.yellow,
+                        height: 60.0,
+                        width: 60.0,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                      ),
+                      Container(
+                        color: Colors.amber,
+                        height: 40.0,
+                        width: 40.0,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                      ),
+                      Container(
+                         color: Colors.brown,
+                        height: 20.0,
+                        width: 20.0,
+                      ),
+                      Divider(),
+                      Row(
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundColor: Colors.lightGreen,
+                            radius: 100.0,
+                            child: Stack(
+                              children: <Widget>[
+                                Container(
+                                  height: 100.0,
+                                  width: 100.0,
+                                  color: Colors.yellow,
+                                ),
+                                Container(
+                                  height: 60.0,
+                                  width: 60.0,
+                                  color: Colors.amber,
+                                ),
+                                Container(
+                                  height: 40.0,
+                                  width: 40.0,
+                                  color: Colors.brown,
+                                )
+                              ],
+                            ),
+                          ),
+                          Divider(),
+                          Text("End of the line"),
+                        ],
+                      ),
+                      Divider(),
+                      Text("End of the line"),
+                      Text("OR IS IT"),
+                    ],
+                  ),
+                ],
+              );
+  }
+
+  Row _buildHorizontalRow() {
+    return Row(
+                children: <Widget>[
+                  Container(
+                    color: Colors.yellow,
+                    height: 40.0,
+                    width: 40.0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.amber,
+                      height: 40.0,
+                      width: 40.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                  ),
+                  Container(
+                    color: Colors.brown,
+                    height: 40.0,
+                    width: 40.0,
+                  ),
+                ],
+              );
   }
 }
